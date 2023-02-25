cc9933997e9ea26018d20f4c71a9db79a2468d6a
Ntebo23 <rapanyane.ntebo@gmail.com>
2023-02-24 19:09:20 -0800

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,3 +1,4 @@
+#!/bin/bash
 #ifndef MAIN_H
 #define MAIN_H
 #include <stdio.h>
@@ -8,4 +9,3 @@ void print_remaining_days(int month, int day, int year);
 int convert_day(int month, int day);
 
 #endif
-
